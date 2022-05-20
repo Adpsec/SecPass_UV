a0950b4d6158633db88a785dac97e31a2c5465a3
3r1kc <105751881+3r1kc@users.noreply.github.com>
2022-05-20 10:44:26 -0500

    Create PaginaPrincipal.java

---
+++ origen/base de datos/PaginaPrincipal.java
@@ -0,0 +1,19 @@
+package class1;
+
+public class PaginaPrincipal extends javax.swing.JFrame {
+
+    public PaginaPrincipal() {
+        initComponents();
+    }
+
+  @SuppressWarnings("unchecked")
+                             
+    private void initComponents() {
+
+    public static void main(String args[]) {
+        
+        Conexion con = new Conexion();         //Creamos un objeto de tipo conexion
+        con.conectado(); 
+    }
+    }
+}
