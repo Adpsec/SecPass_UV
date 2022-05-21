09446d338b99de6c52ddc4f3a60a2882a8460c66
MagdyelAC <99367866+MagdyelAC@users.noreply.github.com>
2022-05-20 19:56:15 -0500

    Add files via upload

---
+++ src/models/Actions/src/actions/Actions.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actions;
+
+/**
+ *
+ * @author MC
+ */
+public class Actions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        id_action obj=new id_action();
+        Action obj2=new Action();
+        
+
+    }
+    
+}
