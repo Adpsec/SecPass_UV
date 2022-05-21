35fba839da0f6e32dd29efac14ff0c35e87ad9a4
MagdyelAC <99367866+MagdyelAC@users.noreply.github.com>
2022-05-20 19:56:57 -0500

    Add files via upload

---
+++ src/models/Log_of_actions/src/log_of_actions/Log_of_actions.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package log_of_actions;
+
+/**
+ *
+ * @author MC
+ */
+public class Log_of_actions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                id_log obj=new id_log();
+                id_action obj2=new id_action();
+                id_user obj3= new id_user();
+                date_log obj4=new date_log();
+
+    }
+    
+}
