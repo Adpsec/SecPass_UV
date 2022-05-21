7c74554c7c01168cc429d5f2da7d876bdb6d398c
MagdyelAC <99367866+MagdyelAC@users.noreply.github.com>
2022-05-20 19:55:09 -0500

    Add files via upload

---
+++ src/models/Users/src/Users/Users.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Users;
+
+/**
+ *
+ * @author MC
+ */
+public class Users {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        name obj=new name();
+        id_user obj1=new id_user();
+        password_user_hash obj2=new password_user_hash();
+        
+    }
+    
+}
