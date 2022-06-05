b56701b077a0bed1678275f99a3cee2c16ccd721
adpsec <asp.security@protonmail.com>
2022-06-05 14:09:55 -0500

    Cambios en models para datos - v1

---
+++ src/models/Users/User/User.java
@@ -0,0 +1,41 @@
+
+package Users;
+
+public class User{
+
+    private String id_user;
+    private String name;
+    private String password_user_hash;
+
+    public User(String id, String n, String p){
+        id_user = id;
+        name = n;
+        password_user_hash = p;
+    };
+
+    //Los getters y setters son solo guías 
+    public void getId_user(){
+
+    };
+
+    public void setId_user(){
+        
+    };
+
+    public void getName(){
+        
+    };
+
+    public void setName(){
+        
+    };
+
+    public void getPassword_user_hash(){
+        
+    };
+
+    public void setPassword_user_hash(){
+        
+    };
+
+};
