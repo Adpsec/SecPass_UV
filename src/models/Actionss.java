41fb1b5a4d3992b14b88efe7d1708c852eec35c7
MagdyelAC <99367866+MagdyelAC@users.noreply.github.com>
2022-06-06 22:45:31 -0500

    Add files via upload
    Actions

---
+++ src/models/Actionss.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actions;
+
+/**
+ *
+ * @author MC
+ */
+public class Actionss {
+     private String id_action;
+    private String action;
+
+    public Actionss(String id, String a){
+        id_action = id;
+        action = a;
+        
+    };
+
+    //Los getters y setters son solo guías 
+    public void getId_action(){
+
+    };
+
+    public void setId_action(){
+        
+    };
+
+    public void getaction(){
+        
+    };
+
+    public void setaction(){
+        
+    };
+
+};
